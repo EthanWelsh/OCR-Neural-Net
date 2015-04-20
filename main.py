fd31fd068046721d7d3fa25eefd8650290111e38
EthanWelsh <welshej@gmail.com>
2015-04-19 23:36:51 -0400

    training with 4x4 bitmap

--- main.py
+++ main.py
@@ -1,54 +1,42 @@
 from Net import *
 import random
 
+o = [0, 0, 0, 0,
+     0, 0, 0, 0,
+     0, 0, 0, 0,
+     0, 0, 0, 0]
+
+x = [1, 0, 0, 1,
+     0, 1, 1, 0,
+     0, 1, 1, 0,
+     1, 0, 0, 1]
 
 def trainNet(net, inputValues, expectedOutputValues):
     net.feedForward(inputValues)
     net.backProp(expectedOutputValues)
 
-
-def trainII(net):
-    trainNet(net, [1, 1], [0])
-
-
-def trainOO(net):
-    trainNet(net, [0, 0], [0])
-
-
-def trainOI(net):
-    trainNet(net, [0, 1], [1])
-
-
-def trainIO(net):
-    trainNet(net, [1, 0], [1])
-
-
 def printResults(net):
-    net.feedForward([1, 0])
-    print "1 ^ 0 == 1 ==", "{0:.2f}".format(net.getResults()[0])
 
-    net.feedForward([0, 1])
-    print "0 ^ 1 == 1 ==", "{0:.2f}".format(net.getResults()[0])
+    net.feedForward(o)
+    print "O:", "{0:.2f}".format(net.getResults()[0])
 
-    net.feedForward([1, 1])
-    print "1 ^ 1 == 0 ==", "{0:.2f}".format(net.getResults()[0])
+    net.feedForward(x)
+    print "X:", "{0:.2f}".format(net.getResults()[0])
 
-    net.feedForward([0, 0])
-    print "0 ^ 0 == 0 ==", "{0:.2f}".format(net.getResults()[0])
-
-net = Net([2, 3, 3, 1])
+net = Net([16, 4, 4, 4, 1])
 
 print(net)
 
 for i in range(0, 100000):
-    r = random.randint(0, 3)
+    r = random.randint(0, 1)
 
-    if r == 0:      trainII(net)
-    elif r == 1:    trainIO(net)
-    elif r == 2:    trainOI(net)
-    else:           trainOO(net)
+    if r == 0:
+        trainNet(net, o, [0])
+    elif r == 1:
+        trainNet(net, x, [1])
 
-    if i % 10000 == 0:
+
+    if i % 1000 == 0:
         printResults(net)
         print
 
